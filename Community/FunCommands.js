91a5171cb941221c89fd3e9e64c2955cd31397c6
Anime-Dev <31098007+Anime-Dev@users.noreply.github.com>
2017-08-17 14:43:03 +0200

    Another command example

--- Community/FunCommands.js
+++ Community/FunCommands.js
@@ -2,17 +2,23 @@ var PERM = require('../Perms');
 var Fun = function () {
     var that = {
         Register: function (AddCommand) {
-            AddCommand("pet", PERM.permissions.everyone, that.pet);
+            AddCommand("pet", PERM.permissions.rolenames.everyone, that.pet);
+            AddCommand("C#", PERM.permissions.rolenames.everyone, that.cs);
             return that;
         },
         UnRegister: function (RemoveCommand) {
             RemoveCommand("pet", PERM.permissions.everyone, that.pet);
+            RemoveCommand("C#", PERM.permissions.everyone, that.cs);
             return that;
         },
         pet: function (command, args, message) {
             message.delete().catch(console.error);
             message.channel.send(message.member + " pets " + args.join(' '));
-         }
+        },
+        cs: function (command, args, message) {
+            message.delete().catch(console.error);
+            message.channel.send("Why do Java developers wear glasses? Because they don't C#");
+        },
     };
     return that;
 };
