b83c01a20216ac3c51fdd2347e046eb6a74570e9
Anime-Dev <31098007+Anime-Dev@users.noreply.github.com>
2017-08-17 14:54:33 +0200

    Improved module registering

--- Community/Community.js
+++ Community/Community.js
@@ -1,19 +1,34 @@
 var PERM = require('../Perms');
-//var Q = require('q');
+var Q = require('q');
 //var Sample = require('Sample');
 var Fun = require('FunCommands');
+
+
 var register = function () {
     var that = {
+        modules: [],
         Register: function (AddCommand, ModuleHandler) {
+            var RegisterModule = function (m) {
+                that.Modules.push(m);
+                ModuleHandler.AddModule(m);
+            }
+
             //register the module here after require'ing
-            //ModuleHandler.AddModule(Sample());
-            ModuleHandler.AddModule(Fun());
+            //RegisterModule(Sample());
+            RegisterModule(Fun());
+
+
             return that;
         },
-        UnRegister: function (RemoveCommand) {
-            //No need to clean up modules
-            return that;
-        },
+        UnRegister: function (RemoveCommand, ModuleHandler) {
+            //No need to clean up modules manually
+            var tasks = [];
+            for (var i = 0; i < modules.length; i++) {
+                tasks.push(ModuleHandler.RemoveModule(modules[i]));
+            }
+            modules = [];
+            return tasks.reduce(Q.when, Q(true));
+        }
     };
     return that;
 };
