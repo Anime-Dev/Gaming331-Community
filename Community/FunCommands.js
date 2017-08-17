e58b830151dd491bd22ee616f35c4eaf7ff5b4db
Anime-Dev <31098007+Anime-Dev@users.noreply.github.com>
2017-08-17 20:11:49 +0200

    Added example emoji

--- Community/FunCommands.js
+++ Community/FunCommands.js
@@ -19,7 +19,7 @@ var Fun = function () {
         },
         cs: function (command, args, message) {
             message.delete().catch(console.error);
-            message.channel.send("Why do Java developers wear glasses? Because they don't C#");
+            message.channel.send("Why do Java developers wear glasses? Because they don't C# " + PERM.channels.GetEmoji("lul"));
         },
         funpic: function(command,args,message) {
             var images = ["WoW for cats: https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif","bert is gona be pissed: https://i.pinimg.com/736x/da/76/e8/da76e8f92ffa7561bc99be733a714c57--funny-shit-funny-pics.jpg","the cat burnned the charger: https://i.pinimg.com/736x/c1/65/02/c16502e626ed0f8c3537beec610e7ac6--funny-pets-funny-animals.jpg","https://s-media-cache-ak0.pinimg.com/736x/f7/14/a3/f714a35f92de708f56d2aa3bd7a5edbe--evil-cats-candles.jpg"]
