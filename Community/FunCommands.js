ba46bdeec44e65e46d91853cecc5f1bd9fe4ebb4
Anime-Dev <31098007+Anime-Dev@users.noreply.github.com>
2017-08-17 17:45:09 +0200

    Fixed funpic command

--- Community/FunCommands.js
+++ Community/FunCommands.js
@@ -23,8 +23,7 @@ var Fun = function () {
         },
         funpic: function(command,args,message) {
             var images = ["WoW for cats: https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif","bert is gona be pissed: https://i.pinimg.com/736x/da/76/e8/da76e8f92ffa7561bc99be733a714c57--funny-shit-funny-pics.jpg","the cat burnned the charger: https://i.pinimg.com/736x/c1/65/02/c16502e626ed0f8c3537beec610e7ac6--funny-pets-funny-animals.jpg","https://s-media-cache-ak0.pinimg.com/736x/f7/14/a3/f714a35f92de708f56d2aa3bd7a5edbe--evil-cats-candles.jpg"]
-            message.channel.send(images[Math.random(images.length)]);
-            
+            message.channel.send(images[Math.round(Math.random() * images.length)]);
         },
     };
     return that;
