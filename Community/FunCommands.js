1bd2995847d6ea9fd30354493e4da256203b9d66
Ender Ossel <EnderNator10@users.noreply.github.com>
2017-08-17 15:21:23 +0200

    added funpic command
    it shows a random funny picture form a catalog inside its code

--- Community/FunCommands.js
+++ Community/FunCommands.js
@@ -4,6 +4,7 @@ var Fun = function () {
         Register: function (AddCommand) {
             AddCommand("pet", PERM.permissions.rolenames.everyone, that.pet);
             AddCommand("C#", PERM.permissions.rolenames.everyone, that.cs);
+            AddCommand("funpic", PERM.permissions.rolenames.everyone, that.funpic);
             return that;
         },
         UnRegister: function (RemoveCommand) {
@@ -18,6 +19,11 @@ var Fun = function () {
         cs: function (command, args, message) {
             message.delete().catch(console.error);
             message.channel.send("Why do Java developers wear glasses? Because they don't C#");
+        },
+        funpic: function(command,args,message) {
+            var images = ["WoW for cats: https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif","bert is gona be pissed: https://i.pinimg.com/736x/da/76/e8/da76e8f92ffa7561bc99be733a714c57--funny-shit-funny-pics.jpg","the cat burnned the charger: https://i.pinimg.com/736x/c1/65/02/c16502e626ed0f8c3537beec610e7ac6--funny-pets-funny-animals.jpg","https://s-media-cache-ak0.pinimg.com/736x/f7/14/a3/f714a35f92de708f56d2aa3bd7a5edbe--evil-cats-candles.jpg"]
+            message.channel.send(images[Math.random(images.length)]);
+            
         },
     };
     return that;
