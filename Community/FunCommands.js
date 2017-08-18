5c54bd5afb2558286daa4446ba5c0b6acf2464f9
Vyolex <vyolex@outlook.com>
2017-08-18 10:06:37 +0200

    Fixed typos in funpic command

--- Community/FunCommands.js
+++ Community/FunCommands.js
@@ -30,7 +30,7 @@ var Fun = function () {
             message.channel.send("Why do Java developers wear glasses? Because they don't C# " + PERM.channels.GetEmoji("lul"));
         },
         funpic: function(command,args,message) {
-            var images = ["WoW for cats: https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif","bert is gona be pissed: https://i.pinimg.com/736x/da/76/e8/da76e8f92ffa7561bc99be733a714c57--funny-shit-funny-pics.jpg","the cat burnned the charger: https://i.pinimg.com/736x/c1/65/02/c16502e626ed0f8c3537beec610e7ac6--funny-pets-funny-animals.jpg","https://s-media-cache-ak0.pinimg.com/736x/f7/14/a3/f714a35f92de708f56d2aa3bd7a5edbe--evil-cats-candles.jpg"]
+            var images = ["WoW for cats: https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif","Bert is gonna be pissed: https://i.pinimg.com/736x/da/76/e8/da76e8f92ffa7561bc99be733a714c57--funny-shit-funny-pics.jpg","The cat burned the charger: https://i.pinimg.com/736x/c1/65/02/c16502e626ed0f8c3537beec610e7ac6--funny-pets-funny-animals.jpg","https://s-media-cache-ak0.pinimg.com/736x/f7/14/a3/f714a35f92de708f56d2aa3bd7a5edbe--evil-cats-candles.jpg"]
             message.channel.send(images[Math.round(Math.random() * images.length)]);
         },
         pantsu: function (command, args, message) {
