cbad4924fc41ad81ed32443493eadbbfcbe7aa26
Anime-Dev <31098007+Anime-Dev@users.noreply.github.com>
2017-08-20 13:06:00 +0200

    Added new sample and new methods in modulehandler for "new" command system

--- Community/Sample.js
+++ Community/Sample.js
@@ -1,20 +1,35 @@
 var PERM = require('../Perms');
+//var ALIAS = require('../Alias');
 //var Q = require('q');
-var sample = function () {
+module.exports = function () {
+    var sample = {
+        Name: "sample",
+        Usage: "$sample <arg1> <arg2>",//Optional defaults to $Name
+        Description: "Does absolutely nothing",//Optional defaults to Name
+        KeepMessage: true,//Optional defaults to false, doesn't delete the messagee automatically
+        WholeArgs: true,//Optional defaults to false, gives arguments as string ("arg1 arg2") instead of an array (["arg1","arg2"])
+        Permissions: PERM.permissions.rolenames.Hosts,//Optional defaults to PERM.permissions.rolenames.everyone
+        Function: function (command, args, message) {
+            //do stuff
+        },
+        Alias: null,//Optional defaults to null, usage: ALIAS([["from1", "to1"], ["from2", "to2"]])
+        Channels: [PERM.channels.channelnames.botchannel],//Optional defaults to null
+    };
     var that = {
-        Register: function (AddCommand) {
+        ModuleName: "sampleModule",
+        Register: function (AddCommand, ModuleHandler) {
             //this will make a command $command that runs the f function when anyone (everyone+) uses the command in botstuff
             //it will also convert the word "from" in the arguments to "to" in the arguments given to function f
-            AddCommand("command", PERM.permissions.rolenames.everyone, that.f, [PERM.config.channels.botchannel], [["from", "to"]]);
+            //AddCommand("command", PERM.permissions.rolenames.everyone, that.f, [PERM.config.channels.botchannel], [["from", "to"]]);
+            ModuleHandler.Add(sample);
             return that;
         },
-        UnRegister: function (RemoveCommand) {
+        UnRegister: function (RemoveCommand, ModuleHandler) {
             //ALWAYS clean up commands
-            RemoveCommand("command", PERM.permissions.rolenames.everyone, that.f);
+            //RemoveCommand("command", PERM.permissions.rolenames.everyone, that.f);
+            ModuleHandler.Remove(sample);
             return that;
         },
-        f: function (command, args, message) { }
     };
     return that;
 };
-module.exports = sample;
--- ModuleHandler.js
+++ ModuleHandler.js
@@ -4,6 +4,7 @@ var ModuleHandler = {
     },
     AddCommand(Name, Perms, func, channels, alias) {
         //adds a command with given name, permissions, function to run, and optional allowed channels (use "*" for all and "!" + channelname for not this one) and aliasses for arguments
+        //THIS WILL SOON BE DEPRICATED! (for now you can still use it but it won't have all functionality)
     },
     RemoveModule(Module) {
         //du-uh
@@ -14,7 +15,24 @@ var ModuleHandler = {
     RemoveCommand(name, perm, func) {
         //you still reading this?
     },
+    Remove(com) {
+        //removes a command or module
+    },
     StatusUpdate(){
         //what do you think this does? Alert santa?
+    },
+    Add(com) {
+        //adds a command or module, see Sample.js/Community.js for correct usage
+    },
+    GetFormattedAllowed(user) {
+        //Gives the allowed commands for a member
+    },
+    StatusUpdate() {
+        //Forces a status update
+    },
+    Discord: Discord,//the Discord object
+    Client: Client,//the Discord Client
+    FindModule(name) {
+        //Finds a module by name and returns the module object
     },
 };
