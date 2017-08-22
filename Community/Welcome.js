f932624046fa982b2d3052490aa5a6447604454c
Anime-Dev <31098007+Anime-Dev@users.noreply.github.com>
2017-08-22 13:42:32 +0200

    Fixed welcome

--- Community/Welcome.js
+++ Community/Welcome.js
@@ -12,32 +12,34 @@ module.exports = function () {
     };
     var that = {
         ModuleName: "Welcome",
+        Handler,
         Register: function (Add, AddCommand, ModuleHandler) {
+            Handler = ModuleHandler;
             PERM.channels.guild.client.on("guildMemberAdd", that.MemberAdd);
-            if (PERM.channels.isBeta()){
+            if (PERM.channels.isBeta()) {
                 ModuleHandler.Add(fakeJoin);
             }
             return that;
         },
         UnRegister: function (Remove, RemoveCommand, ModuleHandler) {
             PERM.channels.guild.client.removeListener("guildMemberAdd", that.MemberAdd);
-            if (PERM.channels.isBeta()){
+            if (PERM.channels.isBeta()) {
                 ModuleHandler.Remove(fakeJoin);
             }
             return that;
         },
         MemberAdd: function (member) {
             if (PERM.channels.isBeta()) {
-                var welcomeEmbed = message.getRichEmbed()
+                var welcomeEmbed = Handler.getRichEmbed()
                     .setTitle("Hiya!")
-                    .setDescription("Welcome " + member + " to the testing server! Be sure to read <#348032747404525569>");
-                PERM.channels.GetChannel("general").send(welcomeEmbed);
+                    .setDescription("Welcome " + member + " to the testing server! Be sure to read <#348032747404525569>")
+                    .send(PERM.channels.GetChannel("general"));
             }
             else {
-                var welcomeEmbed = message.getRichEmbed()
+                var welcomeEmbed = Handler.getRichEmbed()
                     .setTitle("Welcome!")
-                    .setDescription(member + ", to see the game channels you need to have the correct roles (`$add <game>`), these will also be used to announce our events. Be sure to read " + PERM.channels.GetChannel("information") + " to get you started here, we hope you enjoy your stay!");
-                PERM.channels.GetChannel("general").send(welcomeEmbed);
+                    .setDescription(member + ", to see the game channels you need to have the correct roles (`$add <game>`), these will also be used to announce our events. Be sure to read " + PERM.channels.GetChannel("information") + " to get you started here, we hope you enjoy your stay!")
+                    .send(PERM.channels.GetChannel("general"));
             }
         }
     };
